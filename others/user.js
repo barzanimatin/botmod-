61bafc1ab9bd1ddb35be7a001a823316fcf6d754
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-23 15:13:17 +0300

    Create user.js

---
+++ others/user.js
@@ -0,0 +1,34 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  cooldown: 8,
+  description: "**all commands Pinky**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setThumbnail(``)
+    .setImage(`https://media.discordapp.net/attachments/848454202514079764/854642997894971392/image0.gif `)
+    .setTitle(`Calms Is One`)
+    .setDescription(`
+${PREFIX}user - ${PREFIX}music
+${PREFIX}Fun - ${PREFIX}gifs
+${PREFIX}other
+
+`)
+
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+   message.react("✅")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
