43b61857e5e0db2bbbe0e58b0d0ff813b5012f5f
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-23 15:25:34 +0300

    Create music.js

---
+++ others/music.js
@@ -0,0 +1,42 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  cooldown: 8,
+  description: "**all commands Pinky**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setThumbnail(``)
+    .setImage(`https://media.discordapp.net/attachments/848454202514079764/854642997894971392/image0.gif `)
+    .setTitle(`Calms Is One`)
+    .setDescription(`
+***music commnads***
+
+${PREFIX}play - ${PREFIX}skip
+${PREFIX}skipto - ${PREFIX}nowplaying
+${PREFIX}stop - ${PREFIX}volume
+${PREFIX}nowplaying - ${PREFIX}resume
+${PREFIX}shuffle - ${PREFIX}search
+${PREFIX}remove - ${PREFIX}queue 
+${PREFIX}filter - ${PREFIX}radio
+${PREFIX}loop - ${PREFIX}lyrics
+
+
+`)
+
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+   message.react("✅")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
